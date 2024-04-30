c7dd0dce8fee0ce5e1b31982ec5bb50ed77eb84f
lutianyu <wb_lutianyu@meituan.com>
2024-04-30 16:23:29 +0800

    xx

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,8 @@
+/*
+ * @Author: 卢天宇
+ * @Date: 2024-04-29 19:10:34
+ * @Description: commitlint 配置文件
+ */
 module.exports = {
-	extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional']
 };
---
+++ .lintstagedrc.mjs
@@ -0,0 +1,8 @@
+/*
+ * @Author: 卢天宇
+ * @Date: 2024-04-30 16:21:52
+ * @Description: lint-staged 配置文件
+ */
+module.exports = {
+  '*.(js|ts|jsx|tsx)': ['prettier', 'eslint']
+};
