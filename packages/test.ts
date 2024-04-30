1165100c5d5e58c69e12ffb3f1793a77dfe595a5
lutianyu <wb_lutianyu@meituan.com>
2024-04-30 16:31:23 +0800

    xxx

--- packages/test.ts
+++ packages/test.ts
@@ -1,3 +1,8 @@
+/*
+ * @Author: 卢天宇
+ * @Date: 2024-04-29 19:06:02
+ * @Description:
+ */
 const a = 12;
 
 function sum(a: number, b: number): number {
@@ -7,5 +12,5 @@ function sum(a: number, b: number): number {
 const s = sum(1, 2);
 
 console.log(a, s);
-
+console.log('xx');
 export default {};
